2d5150bea4bcfda064cb9f9a4f226a691f31a636
cmpons <cmpons@gmail.com>
2024-11-28 09:54:57 +0100

    Added tree style listing for file explorer  (netrw)

--- lua/file_explorer.lua
+++ lua/file_explorer.lua
@@ -3,6 +3,8 @@ vim.g.netrw_banner = false
 -- are hidden by default
 -- SEE: help netrw-edithide
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
+-- Tree style listing
+vim.g.netrw_liststyle = 3
 
 vim.keymap.set("n", "<Leader>f", "", { desc = "Files" })
 vim.keymap.set("n", "<Leader>fe", function()
