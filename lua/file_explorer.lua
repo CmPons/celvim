8fdc588f86a3d80d0065e1dadd681a74ea8f6360
cmpons <cmpons@gmail.com>
2025-01-02 01:51:06 +0100

    Split file explorer to the left, close it when we leave the buffer

--- lua/file_explorer.lua
+++ lua/file_explorer.lua
@@ -4,11 +4,25 @@ vim.g.netrw_banner = false
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
 -- Tree style listing
 vim.g.netrw_liststyle = 3
+-- 20% of screen
+vim.g.netrw_winsize = 20
+-- Open file in new tab
+vim.g.netrw_browse_split = 3
 
 vim.keymap.set("n", "<Leader>f", "", { desc = "Files" })
 vim.keymap.set("n", "<Leader>fe", function()
-	vim.cmd.tabnew()
-	vim.cmd.Explore()
+	vim.cmd.Vexplore()
 
 	vim.keymap.set("n", "<esc>", ":q<enter>", { buffer = 0 })
+
+	-- All of this is here to close the netrw pane when we select a file
+	local netrw_buf = vim.api.nvim_get_current_buf()
+	vim.api.nvim_create_autocmd({ "BufLeave" }, {
+		callback = function()
+			vim.schedule(function()
+				vim.api.nvim_buf_delete(netrw_buf, {})
+			end)
+		end,
+		buffer = netrw_buf,
+	})
 end, { desc = "File Explorer" })
