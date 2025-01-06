4aa888765519c6f0a57fe57d434f3faf12a4dec2
cmpons <cmpons@gmail.com>
2025-01-06 19:13:17 +0100

    Add two basic keymaps for workspace and document symbols, to be refined  later

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,32 +2,41 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<Leader>c", "", { desc = "Config" })
 vim.keymap.set("n", "<Leader>cr", function()
-	vim.cmd.Reload()
+  vim.cmd.Reload()
 end, { desc = "Reload" })
 
 -- Buffers
 vim.keymap.set("n", "<Leader>b", "", { desc = "Buffer" })
 vim.keymap.set("n", "<Leader>bd", function()
-	vim.cmd.tabclose()
+  vim.cmd.tabclose()
 end, { desc = "Close Buffer" })
 
 vim.keymap.set("n", "<Leader>bo", function()
-	vim.cmd.tabonly()
+  vim.cmd.tabonly()
 end, { desc = "Close Other Buffers" })
 
 vim.keymap.set("n", "<s-H>", function()
-	vim.cmd("tabprev")
+  vim.cmd("tabprev")
 end, { desc = "Reload" })
 
 vim.keymap.set("n", "<s-L>", function()
-	vim.cmd("tabnext")
+  vim.cmd("tabnext")
 end, { desc = "Reload" })
 
 -- LSP
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find References" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Find Definitions" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Find Implementations" })
-vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { desc = "Code Action" })
+
+vim.keymap.set("n", "<leader>sS", function()
+  vim.lsp.buf.workspace_symbol()
+end, { desc = "List workspace symbols" })
+
+vim.keymap.set("n", "<leader>ss", function()
+  vim.lsp.buf.document_symbol()
+end, { desc = "List buffer symbols" })
+
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 
 -- Window switching
 vim.keymap.set("n", "<C-j>", "<C-w>j")
--- lua/quick_fix.lua
+++ lua/quick_fix.lua
@@ -6,115 +6,121 @@ M.qf_win = nil
 local quick_fix_funcs = vim.api.nvim_create_augroup("QuickFixFuncs", {})
 
 local function setup_quick_fix()
-	vim.api.nvim_create_autocmd("BufWinEnter", {
-		group = quick_fix_funcs,
-		callback = function()
-			local win_type = vim.fn.win_gettype()
+  vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = quick_fix_funcs,
+    callback = function()
+      local win_type = vim.fn.win_gettype()
 
-			if win_type == "quickfix" then
-				vim.cmd(":hi QuickFixLine NONE")
-				vim.cmd(":hi qfLineNr NONE")
-				vim.keymap.set("n", "q", ":q<enter>", { buffer = vim.api.nvim_get_current_buf() })
+      if win_type == "quickfix" then
+        vim.cmd(":hi QuickFixLine NONE")
+        vim.cmd(":hi qfLineNr NONE")
+        vim.keymap.set("n", "q", function()
+          vim.api.nvim_win_close(M.qf_win, false)
+          vim.api.nvim_win_close(M.preview_win, false)
+          M.qf_win = nil
+          M.preview_buf = nil
+          M.preview_buf = nil
+        end, { buffer = vim.api.nvim_get_current_buf() })
 
-				M.qf_win = vim.api.nvim_get_current_win()
-				local config = {
-					relative = "editor",
-					row = 25,
-					col = 10,
-					width = 125,
-					height = 5,
-					border = "single",
-					style = "minimal",
-				}
-				vim.api.nvim_win_set_config(0, config)
+        M.qf_win = vim.api.nvim_get_current_win()
+        local config = {
+          relative = "editor",
+          row = 25,
+          col = 10,
+          width = 125,
+          height = 5,
+          border = "single",
+          style = "minimal",
+        }
+        vim.api.nvim_win_set_config(0, config)
 
-				vim.api.nvim_create_autocmd({ "CursorMoved" }, {
-					buffer = vim.api.nvim_get_current_buf(),
-					group = quick_fix_funcs,
-					callback = function()
-						if M.preview_buf == nil and M.preview_win == nil then
-							M.preview_buf = vim.api.nvim_create_buf(false, true)
-							local prev_config = {
-								relative = "editor",
-								row = 3,
-								col = 10,
-								width = 125,
-								height = 20,
-								border = "single",
-								style = "minimal",
-							}
-							M.preview_win = vim.api.nvim_open_win(M.preview_buf, false, prev_config)
-						end
+        vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+          buffer = vim.api.nvim_get_current_buf(),
+          group = quick_fix_funcs,
+          callback = function()
+            if M.preview_buf == nil and M.preview_win == nil then
+              M.preview_buf = vim.api.nvim_create_buf(false, true)
+              local prev_config = {
+                relative = "editor",
+                row = 3,
+                col = 10,
+                width = 125,
+                height = 20,
+                border = "single",
+                style = "minimal",
+              }
+              M.preview_win = vim.api.nvim_open_win(M.preview_buf, false, prev_config)
+            end
 
-						local line = vim.split(vim.api.nvim_get_current_line(), "|")
-						local file = line[1]
-						local lines = {}
-						for file_line in io.lines(file) do
-							lines[#lines + 1] = file_line
-						end
-						vim.api.nvim_buf_set_lines(M.preview_buf, 0, -1, false, lines)
+            local line = vim.split(vim.api.nvim_get_current_line(), "|")
+            local file = line[1]
+            local lines = {}
+            for file_line in io.lines(file) do
+              lines[#lines + 1] = file_line
+            end
+            vim.api.nvim_buf_set_lines(M.preview_buf, 0, -1, false, lines)
 
-						local utils = require("utils")
-						local file_type = utils.get_filetype(file)
-						if file_type ~= nil then
-							vim.bo[M.preview_buf].filetype = file_type
-							vim.bo[M.preview_buf].syntax = utils.get_syntax_from_filetype(file_type)
-						end
+            local utils = require("utils")
+            local file_type = utils.get_filetype(file)
+            if file_type ~= nil then
+              vim.bo[M.preview_buf].filetype = file_type
+              vim.bo[M.preview_buf].syntax = utils.get_syntax_from_filetype(file_type)
+            end
 
-						local cursor = vim.split(line[2], " ")
-						local row, col = tonumber(cursor[1]), tonumber(cursor[3])
-						vim.api.nvim_win_set_cursor(M.preview_win, { row, col })
-						vim.api.nvim_buf_add_highlight(M.preview_buf, -1, "BufferVisible", row - 1, 0, -1)
-					end,
-				})
-			end
-		end,
-	})
+            local cursor = vim.split(line[2], " ")
+            local row, col = tonumber(cursor[1]), tonumber(cursor[3])
+            vim.api.nvim_win_set_cursor(M.preview_win, { row, col })
+            vim.api.nvim_buf_add_highlight(M.preview_buf, -1, "BufferVisible", row - 1, 0, -1)
+          end,
+        })
+      end
+    end,
+  })
 
-	vim.api.nvim_create_autocmd({ "BufLeave" }, {
-		group = quick_fix_funcs,
-		callback = function()
-			if vim.api.nvim_get_current_win() == M.qf_win then
-				vim.schedule(function()
-					if M.qf_win ~= nil then
-						pcall(vim.api.nvim_win_close, M.qf_win, false)
-					end
+  vim.api.nvim_create_autocmd({ "BufLeave" }, {
+    group = quick_fix_funcs,
+    callback = function()
+      if vim.api.nvim_get_current_win() == M.qf_win then
+        vim.schedule(function()
+          if M.qf_win ~= nil then
+            pcall(vim.api.nvim_win_close, M.qf_win, false)
+          end
 
-					if M.preview_win ~= nil then
-						pcall(vim.api.nvim_win_close, M.preview_win, false)
-					end
+          if M.preview_win ~= nil then
+            pcall(vim.api.nvim_win_close, M.preview_win, false)
+          end
 
-					M.qf_win = nil
-					M.preview_win = nil
-					M.preview_buf = nil
-				end)
-			end
-		end,
-	})
+          M.qf_win = nil
+          M.preview_win = nil
+          M.preview_buf = nil
+        end)
+      end
+    end,
+  })
 
-	vim.api.nvim_create_autocmd("WinClosed", {
-		group = quick_fix_funcs,
-		callback = function()
-			if vim.api.nvim_get_current_win() == M.qf_win then
-				vim.schedule(function()
-					if M.preview_win ~= nil then
-						pcall(vim.api.nvim_win_close, M.preview_win, false)
-					end
+  vim.api.nvim_create_autocmd("WinClosed", {
+    group = quick_fix_funcs,
+    callback = function()
+      if vim.api.nvim_get_current_win() == M.qf_win then
+        vim.schedule(function()
+          if M.preview_win ~= nil then
+            pcall(vim.api.nvim_win_close, M.preview_win, false)
+          end
 
-					M.qf_win = nil
-					M.preview_win = nil
-					M.preview_buf = nil
-				end)
-			end
-		end,
-	})
+          M.qf_win = nil
+          M.preview_win = nil
+          M.preview_buf = nil
+        end)
+      end
+    end,
+  })
 end
 
 M.Init = function()
-	setup_quick_fix()
+  setup_quick_fix()
 end
 M.Cleanup = function()
-	vim.api.nvim_clear_autocmds({ group = quick_fix_funcs })
+  vim.api.nvim_clear_autocmds({ group = quick_fix_funcs })
 end
 
 return M
