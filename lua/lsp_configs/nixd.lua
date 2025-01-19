d599d3e76fee8cbb748cd024f8c8f2ef2895ad41
cmpons <cmpons@gmail.com>
2025-01-19 12:41:09 +0100

    Minor fixes to nixd config

--- lua/lsp_configs/nixd.lua
+++ lua/lsp_configs/nixd.lua
@@ -3,28 +3,29 @@ M = {}
 M.filetype = "nix"
 M.file_ext = "*.nix"
 M.config = {
-	name = "nixd",
-	cmd = { "nixd" },
-	root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
+  name = "nixd",
+  cmd = { "nixd" },
+  root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
 
-	settings = {
-		nixd = {
-			nixpkgs = {
-				expr = "import <nixpkgs> { }",
-			},
-			formatting = {
-				command = "nixfmt",
-			},
-			options = {
-				nixos = {
-					expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations.desktop.options',
-				},
-				home_manager = {
-					expr = '(builtins.getFlake "/etc/nixos").homeConfigurations.desktop.options',
-				},
-			},
-		},
-	},
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+      formatting = {
+        command = { "nixfmt" },
+      },
+      options = {
+        nixos = {
+          expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations.desktop.options',
+        },
+        -- Does not seem to work since I use home-manager as a Nixos module!
+        -- home_manager = {
+        -- 	expr = '(builtins.getFlake "/etc/nixos").homeConfigurations.desktop.options',
+        -- },
+      },
+    },
+  },
 }
 
 return M
