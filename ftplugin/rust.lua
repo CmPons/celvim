947ec09bca5bc0fb12e1b051838582f86d2914df
cmpons <cmpons@gmail.com>
2025-02-04 18:54:30 +0100

    Fix being able to execute cargo run multiple times by making RunData global

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -4,7 +4,8 @@ local utils = require("utils")
 
 local root_dir = vim.fs.dirname(vim.fs.find({ "Cargo.lock" }, { upward = true })[1]) or "."
 
-local run_data = {
+-- Must be global so the cargo run process is not per buffer
+RunData = {
 	win = nil,
 	buf = nil,
 	system_obj = nil,
@@ -13,31 +14,31 @@ local run_data = {
 }
 
 local function close_run_win()
-	if run_data.win ~= nil then
-		vim.api.nvim_win_close(run_data.win, false)
+	if RunData.win ~= nil then
+		vim.api.nvim_win_close(RunData.win, false)
 	end
 
-	if run_data.buf ~= nil then
-		vim.api.nvim_buf_delete(run_data.buf, { force = true })
+	if RunData.buf ~= nil then
+		vim.api.nvim_buf_delete(RunData.buf, { force = true })
 	end
 
-	run_data.win = nil
-	run_data.buf = nil
+	RunData.win = nil
+	RunData.buf = nil
 end
 
 local function cleanup_run_data()
 	close_run_win()
-	run_data.system_obj = nil
-	run_data.output = {}
-	run_data.term_channel = nil
+	RunData.system_obj = nil
+	RunData.output = {}
+	RunData.term_channel = nil
 end
 
 local function open_run_output()
-	run_data.buf = vim.api.nvim_create_buf(false, true)
-	pcall(vim.api.nvim_buf_set_name, run_data.buf, "Cargo Run Output")
+	RunData.buf = vim.api.nvim_create_buf(false, true)
+	pcall(vim.api.nvim_buf_set_name, RunData.buf, "Cargo Run Output")
 
-	vim.keymap.set("n", "q", close_run_win, { buffer = run_data.buf, nowait = true })
-	vim.keymap.set("n", "<esc>", close_run_win, { buffer = run_data.buf, nowait = true })
+	vim.keymap.set("n", "q", close_run_win, { buffer = RunData.buf, nowait = true })
+	vim.keymap.set("n", "<esc>", close_run_win, { buffer = RunData.buf, nowait = true })
 
 	local pos = utils.pos_from_screen_percent({ row = 0.10, col = 0.10 })
 	local size = utils.size_from_screen_percent({ row = 0.8, col = 0.8 })
@@ -52,26 +53,26 @@ local function open_run_output()
 		style = "minimal",
 		title = "Output",
 	}
-	run_data.win = vim.api.nvim_open_win(run_data.buf, true, config)
-	run_data.term_channel = vim.api.nvim_open_term(run_data.buf, {})
-	for _, line in ipairs(run_data.output) do
+	RunData.win = vim.api.nvim_open_win(RunData.buf, true, config)
+	RunData.term_channel = vim.api.nvim_open_term(RunData.buf, {})
+	for _, line in ipairs(RunData.output) do
 		if line ~= "" then
-			vim.api.nvim_chan_send(run_data.term_channel, line .. "\n")
+			vim.api.nvim_chan_send(RunData.term_channel, line .. "\n")
 		end
 	end
 
-	local line_count = vim.api.nvim_buf_line_count(run_data.buf)
-	vim.api.nvim_win_set_cursor(run_data.win, { line_count, 1 })
+	local line_count = vim.api.nvim_buf_line_count(RunData.buf)
+	vim.api.nvim_win_set_cursor(RunData.win, { line_count, 1 })
 end
 
 local function on_run_output(_, data)
-	if run_data.system_obj == nil or data == nil or data == "" then
+	if RunData.system_obj == nil or data == nil or data == "" then
 		return
 	end
 
 	local lines = vim.split(data, "\n", { plain = true, trimempty = true })
 	for _, line in ipairs(lines) do
-		run_data.output[#run_data.output + 1] = line
+		RunData.output[#RunData.output + 1] = line
 	end
 end
 
@@ -87,15 +88,15 @@ local function on_run_done(system_obj)
 end
 
 local function cargo_run()
-	if run_data.system_obj then
+	if RunData.system_obj then
 		vim.notify("Stopping running process...")
-		run_data.system_obj:kill(15)
+		RunData.system_obj:kill(15)
 		cleanup_run_data()
 		return
 	end
 
 	vim.notify("Executing Cargo run")
-	run_data.system_obj = vim.system({ "cargo", "r" }, { cwd = root_dir, stdout = on_run_output }, on_run_done)
+	RunData.system_obj = vim.system({ "cargo", "r" }, { cwd = root_dir, stdout = on_run_output }, on_run_done)
 end
 
 --- @type fun(out: vim.SystemCompleted)
