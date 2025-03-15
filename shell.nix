eccdb18040f435bbd0df2cc6870dc00fc18c30f7
cmpons <cmpons@gmail.com>
2025-03-15 22:15:05 +0100

    fix: add all tools, lsp and utils used by celvim to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,23 @@
   "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { }, }:
 let
 in pkgs.mkShell {
-  packages = with pkgs; [ neovim nixfmt stylua shfmt omnisharp-roslyn ];
+  packages = with pkgs; [
+    neovim
+
+    # Utils
+    tree
+    fzf
+
+    # LSPs and Formatters
+    cargo
+    clang-tools
+    lua-language-server
+    nixfmt
+    nixd
+    omnisharp-roslyn
+    stylua
+    shfmt
+  ];
 
   shellHook = ''
     export NVIM_APPNAME=celvim
