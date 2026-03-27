17a2f32fdcebe0f084fa7b74a0b2786493bc8487
Christopher Pons <christopher.pons@embark-studios.com>
2026-03-27 12:31:10 +0100

    fix: Use the correct path to clear the lsp log file on startup

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,48 +1,50 @@
 vim.keymap.set("n", "<C-/>", function()
-	vim.cmd.Terminal()
+  vim.cmd.Terminal()
 end, {})
 
 vim.keymap.set("n", "<CS-/>", function()
-	local current_dir = vim.fn.getcwd()
-	vim.cmd.Terminal(current_dir .. "/engine")
+  local current_dir = vim.fn.getcwd()
+  vim.cmd.Terminal(current_dir .. "/engine")
 end, {})
 
 vim.keymap.set("n", "<CA-/>", function()
-	local current_dir = vim.fn.expand("%:p:h")
-	local results = vim.fs.find({ "Cargo.toml", ".git" }, { upward = true, path = current_dir })
-	if #results > 0 then
-		vim.cmd.Terminal(vim.fs.dirname(results[1]))
-	else
-		vim.cmd.Terminal()
-	end
+  local current_dir = vim.fn.expand("%:p:h")
+  local results = vim.fs.find({ "Cargo.toml", ".git" }, { upward = true, path = current_dir })
+  if #results > 0 then
+    vim.cmd.Terminal(vim.fs.dirname(results[1]))
+  else
+    vim.cmd.Terminal()
+  end
 end, {})
 
 vim.cmd([[tnoremap <Esc> <C-\><C-n>]])
 
 vim.api.nvim_create_user_command("Terminal", function(opts)
-	local dir = ""
-	if #opts.fargs > 0 then
-		dir = opts.fargs[1]
-	end
+  local dir = ""
+  if #opts.fargs > 0 then
+    dir = opts.fargs[1]
+  end
 
-	vim.cmd("tabnew")
+  print("Opening terminal")
 
-	if dir ~= "" then
-		-- Doing this the more complicated way so "zsh" is still the tabname
-		vim.cmd.term("zsh -c 'cd " .. dir .. "; zsh -i'")
-	else
-		vim.cmd.term("zsh")
-	end
+  vim.cmd("tabnew")
 
-	vim.cmd("startinsert")
-	vim.keymap.set("n", "<esc>", ":q<enter>", { buffer = vim.api.nvim_get_current_buf() })
+  if dir ~= "" then
+    -- Doing this the more complicated way so "zsh" is still the tabname
+    vim.cmd.term("zsh -c 'cd " .. dir .. "; zsh -i'")
+  else
+    vim.cmd.term("zsh")
+  end
+
+  vim.cmd("startinsert")
+  vim.keymap.set("n", "<esc>", ":q<enter>", { buffer = vim.api.nvim_get_current_buf() })
 end, { nargs = "?" })
 
 if vim.fn.has("win32") == 1 then
-	local bash_path = vim.fn.exepath("bash")
-	if bash_path ~= "" then
-		vim.o.shell = bash_path
-		vim.o.shellcmdflag = "-c"
-		vim.o.shellxquote = ""
-	end
+  local bash_path = vim.fn.exepath("bash")
+  if bash_path ~= "" then
+    vim.o.shell = bash_path
+    vim.o.shellcmdflag = "-c"
+    vim.o.shellxquote = ""
+  end
 end
