6217aff08bfea9ee1a5753673b3dce25be2fb3b5
ChrisP <cmpons@gmail.com>
2026-03-28 13:40:19 +0100

    chore: Reformat lua config with stylua

--- lua/p4.lua
+++ lua/p4.lua
@@ -1,64 +1,73 @@
 --- @param file_to_edit string
 function EditFile(file_to_edit)
-  local exists = vim.fn.filereadable(file_to_edit)
-  if not exists then
-    error("Failed to open file for edit in P4! File " .. file_to_edit .. " does not exist!")
-    return
-  end
+	local exists = vim.fn.filereadable(file_to_edit)
+	if not exists then
+		error("Failed to open file for edit in P4! File " .. file_to_edit .. " does not exist!")
+		return
+	end
 
-  local cmd = { "p4", "edit", file_to_edit }
+	local cmd = { "p4", "edit", file_to_edit }
 
-  local system_obj = vim.system(cmd, { text = true, cwd = vim.fn.getcwd() })
-  local one_second = 1000
-  local sys_completed = system_obj:wait(one_second)
+	local system_obj = vim.system(cmd, { text = true, cwd = vim.fn.getcwd() })
+	local one_second = 1000
+	local sys_completed = system_obj:wait(one_second)
 
-  if sys_completed.code ~= 0 then
-    error(
-      "Failed to open file " .. file_to_edit .. " for edit:\n" .. sys_completed.stdout .. "\n" .. sys_completed.stderr
-    )
-  else
-    vim.notify("Opened " .. vim.fn.fnamemodify(file_to_edit, ":t") .. " for edit in p4!")
-  end
+	if sys_completed.code ~= 0 then
+		error(
+			"Failed to open file "
+				.. file_to_edit
+				.. " for edit:\n"
+				.. sys_completed.stdout
+				.. "\n"
+				.. sys_completed.stderr
+		)
+	else
+		vim.notify("Opened " .. vim.fn.fnamemodify(file_to_edit, ":t") .. " for edit in p4!")
+	end
 end
 
 --- @param file_to_add string
 local function AddFile(file_to_add)
-  local exists = vim.fn.filereadable(file_to_add)
-  if not exists then
-    error("Failed to open file for add in P4! File " .. file_to_add .. " does not exist!")
-    return
-  end
+	local exists = vim.fn.filereadable(file_to_add)
+	if not exists then
+		error("Failed to open file for add in P4! File " .. file_to_add .. " does not exist!")
+		return
+	end
 
-  local cmd = { "p4", "add", file_to_add }
+	local cmd = { "p4", "add", file_to_add }
 
-  local system_obj = vim.system(cmd, { text = true, cwd = vim.fn.getcwd() })
-  local one_second = 1000
-  local sys_completed = system_obj:wait(one_second)
+	local system_obj = vim.system(cmd, { text = true, cwd = vim.fn.getcwd() })
+	local one_second = 1000
+	local sys_completed = system_obj:wait(one_second)
 
-  if sys_completed.code ~= 0 then
-    error(
-      "Failed to add file " .. file_to_add .. " for edit:\n" .. sys_completed.stdout .. "\n" .. sys_completed.stderr
-    )
-  else
-    vim.notify("Added " .. vim.fn.fnamemodify(file_to_add, ":t") .. " to p4!")
-  end
+	if sys_completed.code ~= 0 then
+		error(
+			"Failed to add file "
+				.. file_to_add
+				.. " for edit:\n"
+				.. sys_completed.stdout
+				.. "\n"
+				.. sys_completed.stderr
+		)
+	else
+		vim.notify("Added " .. vim.fn.fnamemodify(file_to_add, ":t") .. " to p4!")
+	end
 end
 
 local M = {}
 
 M.Init = function()
-  vim.keymap.set("n", "<leader>pe", function()
-    local curr_buff = vim.api.nvim_buf_get_name(0)
-    EditFile(curr_buff)
-  end)
+	vim.keymap.set("n", "<leader>pe", function()
+		local curr_buff = vim.api.nvim_buf_get_name(0)
+		EditFile(curr_buff)
+	end)
 
-  vim.keymap.set("n", "<leader>pa", function()
-    local curr_buff = vim.api.nvim_buf_get_name(0)
-    AddFile(curr_buff)
-  end)
+	vim.keymap.set("n", "<leader>pa", function()
+		local curr_buff = vim.api.nvim_buf_get_name(0)
+		AddFile(curr_buff)
+	end)
 end
 
-M.Cleanup = function()
-end
+M.Cleanup = function() end
 
 return M
--- lua/sessions.lua
+++ lua/sessions.lua
@@ -3,68 +3,68 @@ local M = {}
 local session_cmds = vim.api.nvim_create_augroup("session_cmds", { clear = true })
 
 local get_session_filename = function()
-  local cwd = vim.fs.normalize(vim.fn.getcwd())
-  local filename, _ = cwd:gsub("/", "_")
-  filename, _ = filename:gsub(":", "_")
-  filename, _ = filename:gsub("%.", "_")
-  return filename
+	local cwd = vim.fs.normalize(vim.fn.getcwd())
+	local filename, _ = cwd:gsub("/", "_")
+	filename, _ = filename:gsub(":", "_")
+	filename, _ = filename:gsub("%.", "_")
+	return filename
 end
 
 local get_session_folder = function()
-  local home_path = os.getenv("HOME")
-  if home_path == nil then
-    return nil
-  end
+	local home_path = os.getenv("HOME")
+	if home_path == nil then
+		return nil
+	end
 
-  vim.opt.sessionoptions:append("localoptions")
+	vim.opt.sessionoptions:append("localoptions")
 
-  return vim.fs.normalize(home_path .. "/.local/state/celvim/sessions/")
+	return vim.fs.normalize(home_path .. "/.local/state/celvim/sessions/")
 end
 
 local get_session_file_fullpath = function()
-  if get_session_folder() == nil then
-    return nil
-  end
+	if get_session_folder() == nil then
+		return nil
+	end
 
-  return get_session_folder() .. "/" .. get_session_filename() .. ".vim"
+	return get_session_folder() .. "/" .. get_session_filename() .. ".vim"
 end
 
 M.Init = function()
-  vim.api.nvim_create_autocmd("VimLeavePre", {
-    group = session_cmds,
-    callback = function()
-      local session_folder = get_session_folder()
-      if session_folder == nil then
-        return
-      end
+	vim.api.nvim_create_autocmd("VimLeavePre", {
+		group = session_cmds,
+		callback = function()
+			local session_folder = get_session_folder()
+			if session_folder == nil then
+				return
+			end
 
-      vim.opt.sessionoptions:append("localoptions")
+			vim.opt.sessionoptions:append("localoptions")
 
-      vim.fn.mkdir(session_folder, "p")
+			vim.fn.mkdir(session_folder, "p")
 
-      vim.cmd("mksession! " .. get_session_file_fullpath())
-    end,
-  })
+			vim.cmd("mksession! " .. get_session_file_fullpath())
+		end,
+	})
 
-  vim.api.nvim_create_user_command("LoadSession", function()
-    local session_file_fullpath = get_session_file_fullpath()
-    if session_file_fullpath == nil or vim.uv.fs_stat(session_file_fullpath) == nil then
-      error("Failed to load session file")
-      return
-    end
+	vim.api.nvim_create_user_command("LoadSession", function()
+		local session_file_fullpath = get_session_file_fullpath()
+		if session_file_fullpath == nil or vim.uv.fs_stat(session_file_fullpath) == nil then
+			error("Failed to load session file")
+			return
+		end
 
-    session_file_fullpath = vim.fs.normalize(session_file_fullpath)
-    vim.notify("Loading session from " .. session_file_fullpath)
+		session_file_fullpath = vim.fs.normalize(session_file_fullpath)
+		vim.notify("Loading session from " .. session_file_fullpath)
 
-    -- Delete all buffers first
-    vim.cmd("%bd!")
+		-- Delete all buffers first
+		vim.cmd("%bd!")
 
-    vim.cmd.source(session_file_fullpath)
-  end, {})
+		vim.cmd.source(session_file_fullpath)
+	end, {})
 end
 
 M.Cleanup = function()
-  vim.api.nvim_clear_autocmds({ group = session_cmds })
+	vim.api.nvim_clear_autocmds({ group = session_cmds })
 end
 
 return M
